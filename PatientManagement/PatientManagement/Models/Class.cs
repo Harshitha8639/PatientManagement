b1b33acfadb5ce44436bce37a0f37890315bcfa0
H8639 <bonalakrishnakanth1206@gmail.com>
2024-04-16 17:47:34 +0530

    Patient Model Created

---
+++ PatientManagement/PatientManagement/Models/Class.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientManagement.Models
+{
+    public class PatientModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required")]
+        [StringLength(15, ErrorMessage = "Contact number cannot be longer than 15 characters")]
+        public string ContactNumber { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        public string MedicalComments { get; set; }
+
+        public bool AnyMedicationsTaking { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}
