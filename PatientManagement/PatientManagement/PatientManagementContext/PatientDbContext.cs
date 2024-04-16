195e34fabca324739b6ceaf06f7c57e5764cc88f
H8639 <bonalakrishnakanth1206@gmail.com>
2024-04-16 17:55:06 +0530

    Created DbContext

---
+++ PatientManagement/PatientManagement/PatientManagementContext/PatientDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientManagement.Models;
+
+namespace PatientManagement.PatientManagementContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PatientModel> Patients { get; set; }
+    }
+}
